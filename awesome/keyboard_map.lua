0d45db16e211f10b7cf17b7f24f5e83f2dbda619
bertrandvidal <vidal.bertrand@gmail.com>
2015-02-18 14:09:58 +0100

    Awesome: add the pipe to prefix the keyboard layout widget

--- awesome/keyboard_map.lua
+++ awesome/keyboard_map.lua
@@ -4,11 +4,11 @@
     kbdcfg.layout = { "us", "fr" }
     kbdcfg.current = 1  -- us is our default layout
     kbdcfg.widget = widget({ type = "textbox", align = "right" })
-    kbdcfg.widget.text = " " .. kbdcfg.layout[kbdcfg.current] .. " "
+    kbdcfg.widget.text = " | " .. kbdcfg.layout[kbdcfg.current] .. " "
     kbdcfg.switch = function ()
        kbdcfg.current = kbdcfg.current % #(kbdcfg.layout) + 1
        local t = " " .. kbdcfg.layout[kbdcfg.current] .. " "
-       kbdcfg.widget.text = t
+       kbdcfg.widget.text = " |" .. t
        os.execute( kbdcfg.cmd .. t )
     end
     -- Mouse bindings
